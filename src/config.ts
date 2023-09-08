acf1c8b0c8e923e83467c6f1e1371386bd1e5bd6
YU <111847207+YU000jp@users.noreply.github.com>
2023-09-08 20:06:46 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const allDeleteConfirmMessage = "Clear DONE list";
 export const deleteButton = "Delete";
 export const deleteButtonTitle = "Delete this item";
-export const STORAGE_KEY = "daily-kanban-sidebar-english";
+export const STORAGE_KEY = "daily-kanban-english";
